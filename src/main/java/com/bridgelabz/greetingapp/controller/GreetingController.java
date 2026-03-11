6570a16c1b4bea28f765ecd843722de77a5db058
Deeksha <smdeeksha2005@gmail.com>
2026-03-11 14:53:35 +0530

    [Deeksha]: UC5: Get Greeting by ID using REST API

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -3,18 +3,21 @@ package com.bridgelabz.greetingapp.controller;
 import com.bridgelabz.greetingapp.entity.Greeting;
 import com.bridgelabz.greetingapp.service.GreetingService;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 public class GreetingController {
 
-    private final GreetingService greetingService;
-
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
+    @Autowired
+    private GreetingService greetingService;
 
     @PostMapping("/greeting")
     public Greeting saveGreeting(@RequestBody Greeting greeting) {
         return greetingService.saveGreeting(greeting);
+    }
+
+    @GetMapping("/greeting/{id}")
+    public Greeting getGreetingById(@PathVariable Long id) {
+        return greetingService.getGreetingById(id);
     }
 }
--- src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -3,17 +3,19 @@ package com.bridgelabz.greetingapp.service;
 import com.bridgelabz.greetingapp.entity.Greeting;
 import com.bridgelabz.greetingapp.repository.GreetingRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class GreetingService {
 
-    private final GreetingRepository greetingRepository;
-
-    public GreetingService(GreetingRepository greetingRepository) {
-        this.greetingRepository = greetingRepository;
-    }
+    @Autowired
+    private GreetingRepository greetingRepository;
 
     public Greeting saveGreeting(Greeting greeting) {
         return greetingRepository.save(greeting);
+    }
+
+    public Greeting getGreetingById(Long id) {
+        return greetingRepository.findById(id).orElse(null);
     }
 }
