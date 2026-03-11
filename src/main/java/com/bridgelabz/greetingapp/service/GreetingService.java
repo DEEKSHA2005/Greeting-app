1c30763c38737a658cd7511a19bd3e0570aa8eca
Deeksha <smdeeksha2005@gmail.com>
2026-03-11 14:41:04 +0530

    [Deeksha]: UC4: Save Greeting Message to MySQL using JPA

--- src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapp/service/GreetingService.java
@@ -1,24 +1,19 @@
 package com.bridgelabz.greetingapp.service;
 
+import com.bridgelabz.greetingapp.entity.Greeting;
+import com.bridgelabz.greetingapp.repository.GreetingRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
 
-    public String getGreetingMessage(String firstName, String lastName) {
+    private final GreetingRepository greetingRepository;
 
-        if (firstName != null && lastName != null) {
-            return "Hello " + firstName + " " + lastName;
-        }
+    public GreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
-        if (firstName != null) {
-            return "Hello " + firstName;
-        }
-
-        if (lastName != null) {
-            return "Hello " + lastName;
-        }
-
-        return "Hello World";
+    public Greeting saveGreeting(Greeting greeting) {
+        return greetingRepository.save(greeting);
     }
 }
