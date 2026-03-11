8968badda01e60db70e54cef3390c1a78a16e6a8
Deeksha <smdeeksha2005@gmail.com>
2026-03-11 12:21:52 +0530

    [Deeksha]: UC2: Added GreetingService and moved greeting logic to service layer

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping("/greeting")
-    public String getGreeting() {
-        return "Hello World";
+    public String greeting() {
+        return greetingService.getGreetingMessage();
     }
 }
